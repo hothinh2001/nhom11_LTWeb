ac1df7f647f65a10abbc5d64c19a775e168f09b9
Bảo Trân <19130241@st.hcmuaf.edu.vn>
2024-01-01 23:57:30 +0700

    Update DBContext.java

--- web-animal-feed/src/main/java/vn/edu/hcmuaf/fit/webanimalfeed/context/DBContext.java
+++ web-animal-feed/src/main/java/vn/edu/hcmuaf/fit/webanimalfeed/context/DBContext.java
@@ -12,13 +12,19 @@ public class DBContext {
 
     // Insert your other code right after this comment
     // Change/update information of your database connection, DO NOT change name of instance variables in this class
+
 //    private static final String dbName = "animal-feed";
 //    private static final String userID = "root";
 //    private static final String password = "admin";
 
     private static final String dbName = "animal-feed";
     private static final String userID = "root";
-    private static final String password = "admin";
+
+    //private static final String password = "admin";
+
+    private static final String password = "663bhhEFffaH2CEBG6GcHga5cd2EghCc";
+
+
     public static void main(String[] args) {
         try {
             System.out.println(new DBContext().getConnection() + "");
