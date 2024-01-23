e88fbae4a69ba02e24012114912d178d0a9db454
hothinh2001 <19130214@st.hcmuaf.edu.vn>
2024-01-23 16:08:44 +0700

    Edit user information

--- web-animal-feed/src/main/java/vn/edu/hcmuaf/fit/webanimalfeed/controller/EditUserController.java
+++ web-animal-feed/src/main/java/vn/edu/hcmuaf/fit/webanimalfeed/controller/EditUserController.java
@@ -7,8 +7,10 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
+import java.io.File;
 import java.io.IOException;
 
+@MultipartConfig
 @WebServlet(name = "editUser", value = "/editUser")
 public class EditUserController extends HttpServlet {
     @Override
@@ -16,21 +18,35 @@ public class EditUserController extends HttpServlet {
         String name = request.getParameter("HoTen");
         String birthday = request.getParameter("dateOfBirth");
         String gender = request.getParameter("gender");
-        String address =request.getParameter("address");
+        String address = request.getParameter("address");
         String email = request.getParameter("email");
         String phone = request.getParameter("phone");
 
-        UserEditDao uD = new UserEditDao();
-        Users users = uD.getDanhsachUser(request.getParameter("username"));
-        Users u = uD.edit(name, birthday, gender, address, email, phone);
-         HttpSession session = request.getSession();
-         session.setAttribute("USSER", u);
-        response.sendRedirect("user.jsp");
+        Part p = request.getPart("urlImage");
+        String fileName = p.getSubmittedFileName();
+        String path = getServletContext().getRealPath("") + "uploads";
+        System.out.println("realPath = " + path);
+        File file = new File(path);
+        p.write(path + File.separator + fileName);
+
+        HttpSession session = request.getSession();
+        Users u = (Users) session.getAttribute("acc");
+        UserEditDao uDao = new UserEditDao();
+        if (u != null) {
+            int uid = u.getId();
+            Users user = uDao.editUserById(uid, name, birthday, gender, address, email, phone, fileName);
+            session.setAttribute("user", user);
+            request.setAttribute("fileName", fileName);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("loadUser");
+            dispatcher.forward(request, response);
+        } else {
+            response.sendRedirect("Login.jsp");
+        }
 
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request, response);
     }
 }
